16213bb52d6770a4d54bfb30421180913161905c
Laryssa <laryssa.costa@orbistec.com.br>
2018-08-24 08:23:54 -0300

    Adicionando projeto de criptografia: Crypto

--- Crypto/src/project/Crypto.java
+++ Crypto/src/project/Crypto.java
@@ -10,16 +10,40 @@ public class Crypto {
 	    String text= input.nextLine();
 		normalizeText(text);
 		System.out.println(newtext);
-		
+		shiftAlphabet(2);
 	}
 	public static String normalizeText(String textString) {
 	    newtext=textString.replaceAll("\\s","");
 	    newtext = newtext.replaceAll("[. , : ; ’ ” ! ? ( ) ^ ~ / ]", "");
 	    newtext = newtext.toUpperCase();
 	    return newtext;
-
-	    
 	}
+	public static String caesarify(String t, int key) {
+		String textCryp = shiftAlphabet(key);
+		textCryp = t.replace(t,textCryp);
+		return textCryp;				
+	} 
+	public static String shiftAlphabet(int shift) {
+	    int start = 0;
+	    if (shift < 0) {
+	        start = (int) 'Z' + shift + 1;
+	    } else {
+	        start = 'A' + shift;
+	    }
+	    String result = "";
+	    char currChar = (char) start;
+	    for(; currChar <= 'Z'; ++currChar) {
+	        result = result + currChar;
+	    }
+	    if(result.length() < 26) {
+	        for(currChar = 'A'; result.length() < 26; ++currChar) {
+	            result = result + currChar;
+	        }
+	    }
+	    return result;
+	}
+	
+
 	
 
 }
