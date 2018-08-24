60ba0dd4beda8d36395607e309f4fd1459739f0e
Laryssa <laryssa.costa@orbistec.com.br>
2018-08-24 08:50:05 -0300

    Finalizando projeto Crypto

--- Crypto/src/project/Crypto.java
+++ Crypto/src/project/Crypto.java
@@ -9,8 +9,22 @@ public class Crypto {
 		System.out.println("Write your text: ");
 	    String text= input.nextLine();
 		normalizeText(text);
-		System.out.println(newtext);
-		shiftAlphabet(2);
+		System.out.println(text = newtext);
+		System.out.println("Enter with shift value: ");
+		int shiftValue = input.nextInt();
+		System.out.println(caesarify(text,shiftValue));
+		System.out.print("Enter with size to code groups: ");
+		int groupSize= input.nextInt();
+		String codeGroupedString = groupify(text, groupSize);
+		System.out.println("Encrypting string... ");
+        String cypherText = encryptString(text,shiftValue,groupSize);
+        System.out.println(cypherText);
+        ungroupify(codeGroupedString);
+        System.out.println("Decrypting string...");
+        String plainText = decryptString(cypherText,shiftValue);
+        System.out.println(plainText);
+		
+		
 	}
 	public static String normalizeText(String textString) {
 	    newtext=textString.replaceAll("\\s","");
@@ -43,7 +57,39 @@ public class Crypto {
 	    return result;
 	}
 	
+	public static String groupify(String t, int n){
+        String x = "";
+        int count = 0;
+        for(int i = 0; i < t.length(); i++) {
+            if (i % n == 0 && i != 0) {     //It means if iterator value and number of letters value is same
+                x = x + " ";
+            }
+            x = x + t.charAt(i);
+        }
+        for(int i = x.lastIndexOf(" ")+1; i < x.length(); i++) {
+            ++count;
+        }
+        for(int i = n - count; i > 0; i--){
+            x += "x";
+        }
+        return x;
+    }
+	 public static String encryptString(String t, int sValue, int n)
+	    {
+	        String temp = "";
+	        temp = normalizeText(t);
+	        temp = caesarify(t,sValue);
+	        temp = groupify(t,n);
 
-	
+	        return temp;
+	    }
 
+	    public static String ungroupify(String groupedStr){
+	        String temp = groupedStr.replaceAll("[\\sx]", "");
+	        return temp;
+	    }
+
+	    public static String decryptString(String t, int sValue){
+	        return ungroupify(t);
+	}	
 }
